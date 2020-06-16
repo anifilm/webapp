1616098caf362b1e5e16bb06816d94aaaeae4815
anifilm <anifilm02@gmail.com>
2020-06-16 12:15:32 +0900

    study anguler 2020-06-16

--- angular/firstapp/src/app/product-list/product-list.component.ts
+++ angular/firstapp/src/app/product-list/product-list.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 
-import { products } from '../Products';
+import { products } from '../products';
 
 @Component({
-    selector: 'app-product-list',
-    templateUrl: './product-list.component.html',
-    styleUrls: [ './product-list.component.css' ]
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-    products = products;
+  products = products;
 
-    share() {
-        window.alert('The product has been shared!');
-    }
+  share() {
+    window.alert('The product has been shared!');
+  }
 }
+
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- angular/firstapp/src/app/top-bar/top-bar.component.ts
+++ angular/firstapp/src/app/top-bar/top-bar.component.ts
@@ -1,20 +1,19 @@
-import {Component, OnInit} from '@angular/care';
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-top-bar',
-    templateUrl: './top-bar.component.html',
-    styleUrls: [ './top-bar.component.css' ]
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
+
 }
+
 
 /*
 Copyright Google LLC. All Rights Reserved.
