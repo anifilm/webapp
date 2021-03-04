300e297c59f731e02e4b4b0bc8f78397f359f187
anifilm <anifilm02@gmail.com>
2021-03-05 02:22:14 +0900

    study laravel+canvas 2021-03-05

--- laravel/canvas-blog/public/js/app.js
+++ laravel/canvas-blog/public/js/app.js
@@ -34508,6 +34508,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/canvas-ui.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/canvas-ui.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./node_modules/popper.js/dist/esm/popper.js":
 /*!***************************************************!*\
   !*** ./node_modules/popper.js/dist/esm/popper.js ***!
@@ -49658,7 +49671,8 @@ Vue.compile = compileToFunctions;
 /******/ 		
 /******/ 		var deferredModules = [
 /******/ 			["./resources/js/app.js"],
-/******/ 			["./resources/sass/app.scss"]
+/******/ 			["./resources/sass/app.scss"],
+/******/ 			["./resources/sass/canvas-ui.scss"]
 /******/ 		];
 /******/ 		// no chunk on demand loading
 /******/ 		
--- laravel/canvas-blog/webpack.mix.js
+++ laravel/canvas-blog/webpack.mix.js
@@ -14,3 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/canvas-ui/app.js', 'public/js/canvas-ui.js').vue()
+    .sass('resources/sass/canvas-ui.scss', 'public/css/canvas-ui.css');
