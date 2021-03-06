543cf10be5af0c8e3bebf08f05af4c14dbac315d
anifilm <anifilm02@gmail.com>
2021-03-06 23:51:42 +0900

    study php 2021-03-06

--- _study/learn_PHP/php/02_variables.php
+++ _study/learn_PHP/php/02_variables.php
@@ -3,10 +3,30 @@
 // What is a variable
 
 // Variable types
+/*
+    String
+    Integer
+    Float / Double
+    Boolean
+    Null
+    Array
+    Object
+    Resource
+*/
 
 // Declare variables
+$name = 'Zura';
+$age = 28;
+$isMale = true;
+$height = 1.85;
+$salary = null;
 
 // Print the variables. Explain what is concatenation
+echo $name.'<br>';
+echo $age.'<br>';
+echo $isMale.'<br>';
+echo $height.'<br>';
+echo $salary.'<br>';
 
 // Print types of the variables
 
