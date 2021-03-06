6aeb5ab8d8db13a60a2aeb061fa322b7d9b9cb7c
anifilm <anifilm02@gmail.com>
2021-03-06 20:33:25 +0900

    study php 2021-03-06

---
+++ _study/learn_PHP/PHP_course/data_types/associative_array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>PHP Course</title>
+</head>
+<body>
+<?php
+
+// Associative Array
+$cars = array(50.500, 40.700, 60.300);
+
+var_dump($cars);
+
+
+?>
+</body>
+</html>
---
+++ _study/learn_PHP/PHP_course/data_types/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>PHP Course</title>
+</head>
+<body>
+<?php
+
+// VARIABLES
+// is a symbol of name that stands for a value
+
+$name = "Dary";
+$age = 24;
+
+echo "My name is {$name}<br>";
+echo $name." is ".$age." years old"."<br>";
+
+?>
+</body>
+</html>
