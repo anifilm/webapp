345c6851b4cb3ccac58a5780996bffc75b1cf5fd
anifilm <anifilm02@gmail.com>
2021-03-06 14:34:29 +0900

    자료 정리

---
+++ laravel/blog/resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 blocktext-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Do you want to become a developer?
+                </h1>
+                <a href="/bog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
