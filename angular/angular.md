9129198681add64f23a12e63a550257874bf8ee0
anifilm <anifilm02@gmail.com>
2021-03-10 23:27:04 +0900

    study angular 2021-03-10

--- angular/angular.md
+++ angular/angular.md
@@ -1,3 +1,9 @@
+## my-store-app
+
+Angular 가이드 - Angular 시작하기- 첫번째 앱 만들기
+https://angular.kr/start
+
+
 프로젝트 생성하기
 
 $ ng new 프로젝트명
---
+++ angular/my-store-app/src/app/products-alerts/products-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-products-alerts',
+  templateUrl: './products-alerts.component.html',
+  styleUrls: ['./products-alerts.component.css']
+})
+export class ProductsAlertsComponent {
+  @Input() product;
+  @Output() notify = new EventEmitter();
+}
