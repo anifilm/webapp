08d1021d8b7e4e1ccd44fccb841c9c74c168fa39
anifilm <anifilm02@gmail.com>
2021-03-14 14:07:58 +0900

    study django 2021-03-14

--- django/django-todolist/base/views.py
+++ django/django-todolist/base/views.py
@@ -1,13 +1,48 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Task
+
+
+class CustomLoginView(LoginView):
+    pass
+
+
+class RegisterPage(FormView):
+    pass
+
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
+
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
