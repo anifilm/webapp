68c817ef4181e14504416676e6ef0fbcfcf79b85
anifilm <anifilm02@gmail.com>
2021-04-12 14:46:12 +0900

    study algular 2021-04-12

--- angular/my-store-app/src/app/cart/cart.component.ts
+++ angular/my-store-app/src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,13 +10,27 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private FormBuilder: FormBuilder,
+  ) {
+    this.checkoutForm = this.FormBuilder.group({
+      name: '',
+      address: '',
+    });
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData) {
+    // 주문 로직은 여기에 구현
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
 }
