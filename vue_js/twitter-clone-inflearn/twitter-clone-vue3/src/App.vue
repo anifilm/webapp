4f46245929932207cd34bbbc3e82b42e2eeb4b89
anifilm <anifilm02@gmail.com>
2021-06-04 23:34:03 +0900

    study vue.js 2021-06-04

---
+++ vue_js/twitter-clone-inflearn/twitter-clone-vue3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1
+    class="
+      text-lg
+      bg-gray-900
+      text-white
+      ml-10
+      mt-4
+      pl-10
+      pt-3
+      hover:bg-gray-600
+      hover:text-red-300
+    "
+  >
+    Hello world!
+  </h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
