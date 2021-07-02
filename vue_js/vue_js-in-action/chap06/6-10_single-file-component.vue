ddd45301583293eba7344edcafccdba201e964e6
anifilm <anifilm02@gmail.com>
2021-07-02 11:20:07 +0900

    study vue.js 2021-07-02

---
+++ vue_js/vue_js-in-action/chap06/6-10_single-file-component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Vue.js 앱에 오신걸 환영합니다'
+    }
+  }
+}
+</script>
+
+<!-- 해당 컴포넌트만으로 CSS의 적용 범위를 제한한다면 'scoped' 속성을 사용 -->
+<style scoped>
+
+</style>
