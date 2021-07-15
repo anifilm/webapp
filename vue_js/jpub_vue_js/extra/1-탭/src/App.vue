066b8048aa42d6bac91938c9041d923c6a2712cf
anifilm <anifilm02@gmail.com>
2021-07-15 17:44:52 +0900

    study vue.js 2021-07-15

---
+++ vue_js/jpub_vue_js/extra/1-탭/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <index />
+  </div>
+</template>
+
+<script>
+import Index from '@/components/index';
+
+export default {
+  components: {
+    Index
+  }
+}
+</script>
+
+<style scoped>
+@import './css/main.css';
+</style>
