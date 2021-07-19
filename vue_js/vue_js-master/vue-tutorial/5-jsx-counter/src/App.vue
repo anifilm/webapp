120934370e219ff07bc0b5c96f1cbf9e1c316558
anifilm <anifilm02@gmail.com>
2021-07-19 20:17:53 +0900

    study vue.js 2021-07-19

---
+++ vue_js/vue_js-master/vue-tutorial/5-jsx-counter/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <counter />
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Counter
+  }
+}
+</script>
---
+++ vue_js/vue_js-master/vue-tutorial/5-jsx-counter/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'counter',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  // eslint-disable-next-line no-unused-vars
+  render: function (h) {
+    return (
+      <div>
+        <h2>렌더링 함수 - JSX 사용</h2>
+        <button on-click={() => this.counter += 1}>클릭 카운트 증가</button>
+        <p>클릭 횟수: <b>{this.counter}번</b></p>
+      </div>
+    );
+  }
+}
+</script>
