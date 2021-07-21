d0e60859c6f9f44d2da96cd432aaeb84d7a195a0
anifilm <anifilm02@gmail.com>
2021-07-21 21:33:14 +0900

    study vue.js 2021-07-21

---
+++ vue_js/vue_js-master/vue-tutorial/7-todo-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 입력 중인 새로운 태스크를 임시 저장
+      newTaskName: '',
+      // 새로운 태스크와 연결된 레이블 목록을 임시 저장
+      newTaskLabelIds: [],
+      // 입력 중인 새로운 레이블을 임시 저장
+      newLabelText: ''
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+};
+</script>
