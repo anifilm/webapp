cdaadf6bb37535011dc05c442eab49405026f091
anifilm <anifilm02@gmail.com>
2021-08-03 14:42:40 +0900

    study svelte 2021-08-03

---
+++ svelte/svelte-for-beginner/svelte-tutorial/src/App.svelte
@@ -0,0 +1,29 @@
+<script></script>
+
+<main>
+  <div class="active">My name is Beomy.</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+  .active {
+    color: red;
+    font-size: 3em;
+  }
+</style>
