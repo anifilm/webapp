e2fc2a435268e25dc9114c74210b822a3ef02c40
anifilm <anifilm02@gmail.com>
2021-08-08 13:38:37 +0900

    study svelte 2021-08-08

--- svelte/svelte-for-beginner/svelte-tutorial/src/App.svelte
+++ svelte/svelte-for-beginner/svelte-tutorial/src/App.svelte
@@ -1,13 +1,21 @@
 <script>
-  import Parent from './Parent.svelte';
-
-  function handleMessage(event) {
-    alert(event.detail.text);
-  }
+  let name = 'Beomy';
+  let a = 1;
+  let yes = false;
 </script>
 
 <main>
-  <Parent on:message={handleMessage} />
+  <h1>{name}</h1>
+  <input type="text" bind:value={name} />
+  <h1>{a}</h1>
+  <input type="number" bind:value={a} min="0" max="10" />
+  <input type="range" bind:value={a} min="0" max="10" />
+  {#if yes}
+    <h1>YES</h1>
+  {:else}
+    <h1>NO</h1>
+  {/if}
+  <input type="checkbox" bind:checked={yes} /> check
 </main>
 
 <style>
@@ -16,6 +24,12 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
   }
   @media (min-width: 640px) {
     main {
