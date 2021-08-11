033d8beb3858dd60df35d2a5e0026dde8a127434
anifilm <anifilm02@gmail.com>
2021-08-11 12:38:08 +0900

    study svelte 2021-08-11

---
+++ svelte/svelte_CoreAPI/Part04_반응성/3-자바스크립트 Label 구문의 이해/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	// Svelte가 아닌, JavaScript 문법입니다.
+	loop1:
+	for (let i = 1; i < 4; i++) {
+		loop2:
+		for (let j = 1; j < 4; j++) {
+			if (i === 2) break loop1;
+			console.log(`${i} - ${j}`);
+		}
+		console.log('--------');
+	}
+
+	heropy: {
+		console.log('a');
+		break heropy;
+		console.log('b');
+	}
+
+	// Svelte 반응성 구문
+	let count = 0;
+	$: {
+		console.log(count);
+	}
+</script>
+
+<main>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
--- svelte/svelte_CoreAPI/svelte-tutorial/svelte-lecture/src/App.svelte
+++ svelte/svelte_CoreAPI/svelte-tutorial/svelte-lecture/src/App.svelte
@@ -1,41 +1,10 @@
 <script>
-	let name = 'Heropy';
-	let fruits = ['Apple', 'Banana', 'Cherry'];
-	let user = {
-		name: 'Heropy',
-		depth: {
-			a: 'a'
-		},
-		numbers: [1, 2]
-	};
-	let numbers = user.numbers;
 
-	function assign() {
-		name = 'Neo';
-
-		//fruits.push('Orange');
-		//fruits = fruits;
-		fruits = [...fruits, 'Orange'];
-
-		user.name = 'Neo';
-		user.depth.a = 'b';
-		user.numbers.push(3);
-
-		numbers = numbers;
-	}
 </script>
 
 <main>
-	<h2>name: {name}</h2>
-	<h2>fruits: {fruits}</h2>
-	<h2>user name: {user.name}</h2>
-	<h2>user depth a: {user.depth.a}</h2>
-	<h2>user numbers: {user.numbers}</h2>
-	<h2>numbers: {numbers}</h2>
 
-	<button on:click|once={assign}>Assign!</button>
 </main>
-
 
 <style>
 	main {
@@ -44,11 +13,11 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
-	h2 {
-		/*color: #ff3e00;*/
-		/*text-transform: uppercase;*/
-		font-size: 2em;
-		font-weight: 400;
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
 	}
 	@media (min-width: 640px) {
 		main {
