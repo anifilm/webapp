ca00c4ae325099dbd3294fc525b49af64e02bcdd
anifilm <anifilm02@gmail.com>
2021-08-20 22:43:36 +0900

    study react 2021-08-20

---
+++ react/react_using_skill/chap02/02-4-7/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const name = '리액트';
+  return (
+    <div className="react">
+      {name}
+    </div>
+  );
+}
+
+export default App;
--- react/react_using_skill/react-tutorial/2-hello-react/src/App.js
+++ react/react_using_skill/react-tutorial/2-hello-react/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   const name = '리액트';
   return (
-    <div
-      style={{
-        // background-color는 backgroundColor와 같이 -(하이픈)이 사라지고 카멜 표기법으로 작성
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16, // 단위를 생략하면 px로 지정됨
-      }}
-    >
-      {name}
-    </div>
+    <>
+      {/* 주석은 이렇게 작성합니다. */}
+      <div
+        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성 할 수 있습니다.
+      >
+        {name}
+      </div>
+      // 하지만 이런 주석이나 /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
+      <input />
+    </>
   );
 }
 
