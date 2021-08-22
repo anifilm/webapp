9bcf2fd0cb2dce6d420f741d37706ac9b4587f6e
anifilm <anifilm02@gmail.com>
2021-08-22 22:33:29 +0900

    study svelte 2021-08-22

---
+++ svelte/svelte_CoreAPI/Part13_액션/1-사용 패턴 정리/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	function hello(node, color) {
+		console.log(node);
+		node.style.width = '100px';
+		node.style.height = '100px';
+		node.style.backgroundColor = color || 'tomato';
+	}
+</script>
+
+<main>
+	<div use:hello></div>
+	<div use:hello={'royalblue'}></div>
+</main>
---
+++ svelte/svelte_CoreAPI/Part13_액션/1-사용 패턴 정리/src/App2.svelte
@@ -0,0 +1,24 @@
+<script>
+  function hello(node, options = {}) {
+    const {
+      width = '100px',
+      height = '100px',
+      color = 'tomato'
+    } = options;
+    console.log(node);
+    node.style.width = width;
+    node.style.height = height;
+    node.style.backgroundColor = color;
+  }
+</script>
+
+<main>
+  <div use:hello></div>
+  <div
+    use:hello={{
+      width: '200px',
+      height: '70px',
+      color: 'royalblue'
+    }}
+  />
+</main>
