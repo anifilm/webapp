7ede2ab5969734e6b996a713a7c77ff62975a9a7
anifilm <anifilm02@gmail.com>
2021-08-27 19:04:56 +0900

    study svelte 2021-08-27

--- svelte/svelte_CoreAPI/svelte-tutorial/svelte-trello-app/src/components/CreateList.svelte
+++ svelte/svelte_CoreAPI/svelte-tutorial/svelte-trello-app/src/components/CreateList.svelte
@@ -1,6 +1,7 @@
 <script>
   import { tick } from 'svelte';
   import { lists } from '~/store/list';
+  import { autoFocusout } from '~/actions/autoFocusout';
 
   let isEditMode = false;
   let title = '';
@@ -27,7 +28,7 @@
 
 <div class="create-list">
   {#if isEditMode}
-    <div class="edit-mode">
+    <div use:autoFocusout={offEditMode} class="edit-mode">
       <textarea
         bind:value={title}
         bind:this={textareaEl}
