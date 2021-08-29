1ef89536131dd5249ffd0c7454c4ebf69bef1812
anifilm <anifilm02@gmail.com>
2021-08-29 20:28:37 +0900

    study svelte 2021-08-29

---
+++ svelte/svelte_SPA/Part01_Snowpack으로 프로젝트 구성/svelte-movie-app/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from '~/components/Header.svelte';
+</script>
+
+<Header />
+<h1>Svelte!</h1>
+
+<style lang="scss">
+  $color: red;
+  h1 {
+    color: $color;
+  }
+</style>
