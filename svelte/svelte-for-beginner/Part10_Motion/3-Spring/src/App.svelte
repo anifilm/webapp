609d6b11816101ff581dd569ddf679d5336ed500
anifilm <anifilm02@gmail.com>
2021-09-02 15:40:44 +0900

    Update App.svelte

--- svelte/svelte-for-beginner/Part10_Motion/3-Spring/src/App.svelte
+++ svelte/svelte-for-beginner/Part10_Motion/3-Spring/src/App.svelte
@@ -1,16 +1,11 @@
 <script>
   import { spring } from 'svelte/motion';
 
-  const coords = spring(
-    {
-      x: 50,
-      y: 50
-    },
-    {
-      stiffness: 0.1,
-      damping: 0.25
-    }
-  );
+  const coords = spring({ x: 50, y: 50 }, {
+    stiffness: 0.1,
+    damping: 0.25,
+    precision: 0.01
+  });
 
   const size = spring(10);
 </script>
@@ -24,6 +19,10 @@
     <label>
       <h3>damping ({coords.damping})</h3>
       <input bind:value={coords.damping} type="range" min="0" max="1" step="0.01" />
+    </label>
+    <label>
+      <h3>precision ({coords.precision})</h3>
+      <input bind:value={coords.precision} type="range" min="0" max="1" step="0.01" />
     </label>
   </div>
 
