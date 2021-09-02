2ef52f680e18b98868793e0e4b6c537e11585c7f
anifilm <anifilm02@gmail.com>
2021-09-02 20:39:35 +0900

    study svelte 2021-09-02

---
+++ svelte/svelte-for-beginner/Part11_Transition/1-트랜지션 사용하기/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  let visible = true;
+</script>
+
+<main>
+  <label>
+    <input type="checkbox" bind:checked={visible} />
+    visible
+  </label>
+
+  {#if visible}
+    <p transition:fade={{ delay: 500, duration: 1000 }}>
+      Fades in and out
+    </p>
+  {/if}
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
