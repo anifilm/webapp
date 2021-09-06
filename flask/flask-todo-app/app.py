aafb96c7ea6f4b637ce97f0b406a89a6e56e952f
anifilm <anifilm02@gmail.com>
2021-09-06 17:45:55 +0900

    study flask 2021-09-06

--- flask/flask-todo-app/app.py
+++ flask/flask-todo-app/app.py
@@ -11,6 +11,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
+    editable = db.Column(db.Boolean)
 
 
 @app.route("/")
@@ -22,24 +23,53 @@ def home():
 
 @app.route("/add", methods=["POST"])
 def add():
-    # add new item
+    # 아이템 추가
     title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
+    if title.strip() == "":
+        print("작성된 내용이 없습니다.")
+    else:
+        new_todo = Todo(title=title, complete=False, editable=False)
+        db.session.add(new_todo)
+        db.session.commit()
     return redirect(url_for("home"))
 
-@app.route("/edit/<int:todo_id>")
-def edit(todo_id):
-    # edit item
+
+@app.route("/done/<int:todo_id>")
+def done(todo_id):
+    # 진행 상태 변경
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
 
+
+@app.route("/edit/<int:todo_id>")
+def edit(todo_id):
+    # 아이템 수정
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.editable = not todo.editable
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/edit_complete/<int:todo_id>", methods=["POST"])
+def edit_complete(todo_id):
+    # 수정 완료
+    title = request.form.get("title")
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.editable = not todo.editable
+    # 내용이 없거나 수정된 사항이 없다면 home으로 돌아감
+    if title.strip() == "" or todo.title == title:
+        print("수정된 내용이 없습니다.")
+    else:
+        todo.title = title.strip()
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    # delete item
+    # 아이템 삭제
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
@@ -50,8 +80,8 @@ if __name__ == "__main__":
     db.create_all()
 
     # db 테스트용 내용 생성
-    #new_todo = Todo(title="todo 1", complete=False)
-    #db.session.add(new_todo)
-    #db.session.commit()
+    # new_todo = Todo(title="todo 1", complete=False, editable=False)
+    # db.session.add(new_todo)
+    # db.session.commit()
 
     app.run(debug=True)
