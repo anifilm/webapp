c55451f5b1778832e458d3027374f4af63912309
anifilm <anifilm02@gmail.com>
2021-09-17 21:57:32 +0900

    study flask 2021-09-17

--- flask/doit_JumpToFlask/chap01/01-6/pybo.py
+++ flask/doit_JumpToFlask/chap01/01-6/pybo.py
@@ -6,7 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello_pybo():
     return "Hello, Pybo!"
-
-
-if __name__ == "__main__":
-    app.run()
--- flask/doit_JumpToFlask/chap02/02-2/pybo/__init__.py
+++ flask/doit_JumpToFlask/chap02/02-2/pybo/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from .views import main_views
-
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(main_views.bp)
+
+    @app.route("/")
+    def hello_pybo():
+        return "Hello, Pybo!"
+
     return app
