62fb0abe4b0fe6ac4978efa15aeee9dd54d41cac
anifilm <anifilm02@gmail.com>
2021-09-19 12:33:51 +0900

    study flask 2021-09-19

---
+++ flask/doit_JumpToFlask/chap03/03-11/pybo/models.py
@@ -0,0 +1,54 @@
+from pybo import db
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime(), nullable=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
+    user = db.relationship("User", backref=db.backref("question_set"))
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(
+        db.Integer, db.ForeignKey("question.id", ondelete="CASCADE")
+    )
+    question = db.relationship("Question", backref=db.backref("answer_set"))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime(), nullable=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
+    user = db.relationship("User", backref=db.backref("answer_set"))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
+    user = db.relationship("User", backref=db.backref("comment_set"))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime())
+    question_id = db.Column(
+        db.Integer, db.ForeignKey("question.id", ondelete="CASCADE"), nullable=True
+    )
+    question = db.relationship("Question", backref=db.backref("comment_set"))
+    answer_id = db.Column(
+        db.Integer, db.ForeignKey("answer.id", ondelete="CASCADE"), nullable=True
+    )
+    answer = db.relationship("Answer", backref=db.backref("comment_set"))
