ec29b7ef0bdc01a5e149ac7e84e010cbb60bb3bc
anifilm <anifilm02@gmail.com>
2021-09-28 16:37:53 +0900

    study django 2021-09-28

--- django/django-python-web/Part01/chap05/mysite/urls.py
+++ django/django-python-web/Part01/chap05/mysite/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from mysite import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.HomeView.as_view(), name="home"),
     path("polls/", include("polls.urls")),
     path("books/", include("books.urls")),
 ]
