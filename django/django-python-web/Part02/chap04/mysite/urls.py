d5c6c9bc901a1181f822d59226caab795017ccdf
anifilm <anifilm02@gmail.com>
2021-09-28 20:08:22 +0900

    study django 2021-09-28

---
+++ django/django-python-web/Part02/chap04/mysite/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
+]
