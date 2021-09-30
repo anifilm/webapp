f0232b67b8f7e8821c9881d4a1f5e1fe691d9645
anifilm <anifilm02@gmail.com>
2021-09-30 10:12:24 +0900

    study django 2021-09-30

--- django/doit_django_AtoZ/chap08/08-2_FBV로 페이지 만들기/blog/models.py
+++ django/doit_django_AtoZ/chap08/08-2_FBV로 페이지 만들기/blog/models.py
@@ -7,10 +7,11 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     # author: 추후 작성 예정
 
     def __str__(self):
-        return f"{self.pk}. {self.title}"
+        return f"[{self.pk}] {self.title}"
 
     def get_absolute_url(self):
         return f"/blog/{self.pk}/"
