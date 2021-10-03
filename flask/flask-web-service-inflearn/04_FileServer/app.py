9e04b9ee723b64fc50e98c1afd92dd6b356761d7
anifilm <anifilm02@gmail.com>
2021-10-03 18:36:45 +0900

    study flask 2021-10-03

--- flask/flask-web-service-inflearn/04_FileServer/app.py
+++ flask/flask-web-service-inflearn/04_FileServer/app.py
@@ -4,6 +4,10 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
 
+import os
+import datetime
+import time
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
@@ -12,14 +16,43 @@ class FileForm(FlaskForm):
     files = FileField(validators=[FileRequired("업로드할 파일을 넣어주세요.")])
 
 
+def stamp2real(stamp):
+    return datetime.datetime.fromtimestamp(stamp)
+
+
+def info(filename):
+    ctime = os.path.getctime(filename)  # 만든시간
+    mtime = os.path.getmtime(filename)  # 수정시간
+    size = os.path.getsize(filename)    # 파일크기 (단위: bytes)
+    return ctime, mtime, size
+
+
 @app.route("/", methods=["GET", "POST"])
-def upload_page():
+def main_page():
     form = FileForm()
     if form.validate_on_submit():
         f = form.files.data
         f.save("./uploads/" + secure_filename(f.filename))
-        return render_template("check.html")
-    return render_template("upload.html", form=form)
+        return render_template("check.html", pwd=os.getcwd() + "\\uploads")
+
+    filelist = os.listdir("./uploads")
+    infos = []
+
+    for name in filelist:
+        fileinfo = {}
+        ctime, mtime, size = info("./uploads/" + name)
+        fileinfo["name"] = name
+        fileinfo["create"] = stamp2real(ctime)
+        fileinfo["modify"] = stamp2real(mtime)
+        if size <= 1000000:
+            fileinfo["size"] = "%.2f KB" % (size / 1024)
+        else:
+            fileinfo["size"] = "%.2f MB" % (size / (1024.0 * 1024.0))
+        infos.append(fileinfo)
+
+    return render_template(
+        "home.html", form=form, pwd=os.getcwd() + "\\uploads", infos=infos
+    )
 
 
 if __name__ == "__main__":
