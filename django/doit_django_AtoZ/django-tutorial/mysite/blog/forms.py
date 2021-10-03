37194f6f6f429e3ed0f90c48e4610dbec7be6b58
anifilm <anifilm02@gmail.com>
2021-10-04 07:00:27 +0900

    study django 2021-10-04 Comment 관련 수정

--- django/doit_django_AtoZ/django-tutorial/mysite/blog/forms.py
+++ django/doit_django_AtoZ/django-tutorial/mysite/blog/forms.py
@@ -6,6 +6,3 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("content",)
-        widgets = {
-            'content': forms.Textarea(attrs={'rows': 3})
-        }
