78395af321b7ac1408f1f1bfe8de08488f57c50a
anifilm <anifilm02@gmail.com>
2021-10-03 17:15:21 +0900

    study flask 2021-10-03

--- flask/flask-web-service-inflearn/04_FileServer/app.py
+++ flask/flask-web-service-inflearn/04_FileServer/app.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
 
 
-@app.route("/")
+class FileForm(FlaskForm):
+    files = FileField(validators=[FileRequired("업로드할 파일을 넣어주세요.")])
+
+
+@app.route("/", methods=["GET", "POST"])
 def upload_page():
-    return render_template("upload.html")
-
-
-@app.route("/fileUpload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        f = request.files["file"]
+    form = FileForm()
+    if form.validate_on_submit():
+        f = form.files.data
         f.save("./uploads/" + secure_filename(f.filename))
         return render_template("check.html")
-    else:
-        return "Not"
+    return render_template("upload.html", form=form)
 
 
 if __name__ == "__main__":
