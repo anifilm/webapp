4af40f26dcc03ad1872781ccabb14035ca878931
anifilm <anifilm02@gmail.com>
2021-10-03 10:38:45 +0900

    init flask blog project 2021-10-03

---
+++ flask/flask-firebase-blog/DB_hander.py
@@ -0,0 +1,28 @@
+import pyrebase
+import json
+
+
+class DBModule:
+    def __init__(self):
+        with open("./auth/firebaseAuth.json") as file:
+            config = json.load(file)
+
+        self.firebase = pyrebase.initialize_app(config)
+
+    def login(self, id, pwd):
+        pass
+
+    def signin(self, id, pwd, name, email):
+        pass
+
+    def write_post(self, user, content):
+        pass
+
+    def post_list(self):
+        pass
+
+    def post_detail(self, pid):
+        pass
+
+    def get_user(self, uid):
+        pass
---
+++ flask/flask-firebase-blog/app.py
@@ -0,0 +1,3 @@
+from DB_hander import DBModule
+
+DB = DBModule()
