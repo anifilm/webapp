c4009258f778bd7bc46c03df328adbf56a14cff5
anifilm <anifilm02@gmail.com>
2021-10-08 10:25:56 +0900

    Update index.js

--- react/react-blog-inflearn/server/config/index.js
+++ react/react-blog-inflearn/server/config/index.js
@@ -6,7 +6,7 @@ const userId = process.env.USER_ID;
 const userPw = process.env.USER_PW;
 
 export default {
-  MONGO_URI: `mongodb+srv://${userId}:${userPw}@cluster0.yhhud.mongodb.net/my-mongodb?retryWrites=true&w=majority`,
+  MONGO_URI: `mongodb+srv://${userId}:${userPw}@cluster0.hpa4f.mongodb.net/my-mongodb?retryWrites=true&w=majority`,
   JWT_SECRET: process.env.JWT_SECRET,
   PORT: process.env.PORT
 }
