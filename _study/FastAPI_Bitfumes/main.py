b339c9bfa5419a78f501599718e1ba36b141308a
anifilm <anifilm02@gmail.com>
2021-10-09 23:38:27 +0900

    Update main.py

--- _study/FastAPI_Bitfumes/main.py
+++ _study/FastAPI_Bitfumes/main.py
@@ -1,15 +1,13 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index():
+    return {"data": {"name": "anifilm"}}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/about")
+def about():
+    return {"data": {"about page"}}
