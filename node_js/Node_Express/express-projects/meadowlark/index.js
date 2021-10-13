7137dfd1a991daf7ce2c5bbf371f046675b08537
anifilm <anifilm02@gmail.com>
2021-10-14 08:59:24 +0900

    study express 2021-10-14

---
+++ node_js/Node_Express/express-projects/meadowlark/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Meadowlark Travel');
+});
+
+app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About Meadowlark Travel');
+});
+
+// custom 404 page
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(port, () => {
+  console.log(`The serveris running, please open your browser at http://localhost:${port}`);
+});
