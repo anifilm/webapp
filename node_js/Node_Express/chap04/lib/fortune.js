1f02772b2ca567624a57647521456ebd67d03517
anifilm <anifilm02@gmail.com>
2021-10-14 10:48:52 +0900

    study express 2021-10-14

---
+++ node_js/Node_Express/chap04/lib/fortune.js
@@ -0,0 +1,14 @@
+const fortuneCookies = [
+  'Conquer your fears or they will conquer you.',
+  'Rivers need springs.',
+  "Do not fear what you don't know.",
+  'You will have a pleasant surprise.',
+  'Whenever possible, keep it simple.',
+];
+
+const getFortune = () => {
+  const idx = Math.floor(Math.random() * fortuneCookies.length);
+  return fortuneCookies[idx];
+}
+
+export default getFortune;
