bf38150c37b5221d9d21ce7ed9a3950afa0eee92
anifilm <anifilm02@gmail.com>
2021-10-14 11:38:16 +0900

    study express 2021-10-14

---
+++ node_js/Node_Express/chap05/lib/__tests__/handlers.test.js
@@ -0,0 +1,9 @@
+import * as handers from '../handlers';
+
+test('home page renders', () => {
+  const req = {};
+  const res = { render: jest.fn() };
+  handers.home(req, res);
+  expect(res.render.mock.calls.length).toBe(1);
+  expect(res.render.mock.calls[0][0]).toBe('home');
+});
---
+++ node_js/Node_Express/express-projects/meadowlark/lib/handlers.js
@@ -0,0 +1,20 @@
+import getFortune from './fortune';
+
+const home = (req, res) => {
+  res.render('home');
+};
+
+const about = (req, res) => {
+  res.render('about', { fortune: getFortune() });
+};
+
+const notFound = (req, res) => {
+  res.render('404');
+};
+
+const serverError = (err, req, res, next) => {
+  console.log(err);
+  res.render('500');
+};
+
+export { home, about, notFound, serverError };
