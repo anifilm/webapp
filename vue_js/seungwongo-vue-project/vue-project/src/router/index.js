11ca7e84584693b6d3674aee8ee8a5f4a9fa0d18
anifilm <anifilm02@gmail.com>
2021-11-03 19:11:08 +0900

    study vue.js 2021-11-03

--- vue_js/seungwongo-vue-project/vue-project/src/router/index.js
+++ vue_js/seungwongo-vue-project/vue-project/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/views/Home.vue';
 import DataBinding from '@/views/DataBindingList2.vue';
-import Components from '@/views/ParentComponent4.vue'
+import Components from '@/views/ParentComponent5.vue'
 
 const routes = [
   {
---
+++ vue_js/seungwongo-vue-project/vue-project/src/views/ParentComponent5.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button v-on:click="checkChild">자식 컴포넌트 데이터 조회</button>
+    <child-component ref="child_component" />
+  </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent5.vue';
+
+export default {
+  components: { ChildComponent },
+  computed: {
+    msg() {
+      return this.$refs.child_component.msg;
+    }
+  },
+  methods: {
+    checkChild() {
+      alert(this.msg);
+    }
+  }
+};
+</script>
