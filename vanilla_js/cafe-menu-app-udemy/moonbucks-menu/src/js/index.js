20178bdb620cc77ab628c6bae1cb2d919f34daec
anifilm <anifilm02@gmail.com>
2021-11-03 11:20:47 +0900

    Update index.js

--- vanilla_js/cafe-menu-app-udemy/moonbucks-menu/src/js/index.js
+++ vanilla_js/cafe-menu-app-udemy/moonbucks-menu/src/js/index.js
@@ -40,6 +40,16 @@ function App() {
     $('#espresso-menu-name').value = '';
   };
 
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+      const updatedMenuName = prompt('메뉴명을 수정하세요.', $menuName.innerText);
+      $menuName.innerText = updatedMenuName;
+    }
+    if (e.target.classList.contains('menu-remove-button')) {
+    }
+  })
+
   // form 태그가 자동으로 전송되는걸 막아준다.
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
