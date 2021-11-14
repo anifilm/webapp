f526ca5172c1a0b28018d852acb5334c39ce6b5e
anifilm <anifilm02@gmail.com>
2021-11-14 23:30:01 +0900

    study flask 2021-11-14

--- flask/doit_JumpToFlask/flask-tutorial/mysite2/pybo/views/main_views.py
+++ flask/doit_JumpToFlask/flask-tutorial/mysite2/pybo/views/main_views.py
@@ -1,15 +1,8 @@
-from flask import Blueprint, render_template
-from pybo.models import Question
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/hello')
-def hello_world():
-    return 'Hello, world!'
-
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list=question_list)
-
-# Route, Controller 역할을 동시에 수행한다.
+    return redirect(url_for('question._list'))
