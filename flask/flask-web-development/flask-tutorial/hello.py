8ad547a5f3c456f62f23e42f4aa0e9a47e242381
anifilm <anifilm02@gmail.com>
2021-11-17 13:28:32 +0900

    study flask 2021-11-17

---
+++ flask/flask-web-development/flask-tutorial/hello.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def intenal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
