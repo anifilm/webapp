f72da90015289ece96b14e1af80649254505fe6b
anifilm <anifilm02@gmail.com>
2021-12-19 11:54:43 +0900

    study django 2021-12-19

--- django/django-instagram-clone_kindfamily/instaclone-frontend/js/main.js
+++ django/django-instagram-clone_kindfamily/instaclone-frontend/js/main.js
@@ -23,10 +23,113 @@ function deligationFunc(e) {
   }
 
   if (elem.matches('[data-name="heartbeat"]')) {
-    console.log('하트');
+    //console.log('하트');
+    let pk = elem.getAttribute('name');
+
+    $.ajax({
+      Method: 'POST',
+      url: './data/like.json',
+      data: {pk},
+      dataType: 'json',
+      success: function (response) {
+        let likeCount = document.querySelector('#like-count-37');
+        likeCount.innerHTML = '좋아요 ' + response.like_count + '개';
+      },
+      error: function (request, status, error) {
+        alert('로그인이 필요합니다.');
+        window.location.replace('https://www.naver.com');
+      }
+    });
   }
   else if (elem.matches('[data-name="bookmark"]')) {
-    console.log('북마크');
+    //console.log('북마크');
+    let pk = elem.getAttribute('name');
+
+    $.ajax({
+      Method: 'POST',
+      url: './data/bookmark.json',
+      data: {pk},
+      dataType: 'json',
+      success: function (response) {
+        let bookmarkCount = document.querySelector('#bookmark-count-37');
+        bookmarkCount.innerHTML = '북마크 ' + response.bookmark_count + '개';
+      },
+      error: function (request, status, error) {
+        alert('로그인이 필요합니다.');
+        window.location.replace('https://www.naver.com');
+      }
+    });
+  }
+  else if (elem.matches('[data-name="comment"]')) {
+    //console.log('댓글');
+    let content = document.querySelector('#add-comment-post37 > input[type=text]').value;
+    console.log(content);
+
+    if (content.length > 140) {
+      alert('댓글은 최대 140자 입력 가능합니다. 현재 글자수: ' + content.length);
+      return;
+    }
+
+    $.ajax({
+      Method: 'POST',
+      url: './comment.html',
+      data: {
+        'pk': 37, // 'pk': pk
+        'content': content,
+      },
+      dataType: 'html',
+      success: function (data) {
+        document.querySelector('#comment-list-ajax-post37').insertAdjacentHTML('afterbegin', data);
+      },
+      error: function (request, status, error) {
+        alert('문제가 발생했습니다.');
+      }
+    });
+    document.querySelector('#add-comment-post37 > input[type=text]').value = '';
+  }
+  else if (elem.matches('[data-name="comment_delete"]')) {
+    //console.log('댓글 삭제');
+    $.ajax({
+      Method: 'POST',
+      url: './data/delete.json',
+      data: {
+        'pk': 37, // 'pk': pk
+      },
+      dataType: 'json',
+      success: function (response) {
+        if (response.status) {
+          let comment = document.querySelector('.comment-detail');
+          comment.remove();
+        }
+      },
+      error: function (request, status, error) {
+        alert('문제가 발생했습니다.');
+        window.location.replace('https://www.naver.com');
+      }
+    });
+  }
+  else if (elem.matches('[data-name="follow"]')) {
+    console.log('팔로우');
+    $.ajax({
+      Method: 'POST',
+      url: './data/follow.json',
+      data: {
+        'pk': 37, // 'pk': pk
+      },
+      dataType: 'json',
+      success: function (response) {
+        if (response.status) {
+          document.querySelector('input.follow').value = '팔로잉';
+        }
+        else {
+          document.querySelector('input.follow').value = '팔로워';
+        }
+      },
+      error: function (request, status, error) {
+        alert('문제가 발생했습니다.');
+        window.location.replace('https://www.naver.com');
+      }
+    });
   }
   else if (elem.matches('[data-name="share"]')) {
     console.log('공유');
@@ -58,6 +161,12 @@ function resizeFunc() {
 }
 
 function scrollFunc() {
+  let scrollHeight = pageYOffset + window.innerHeight;
+  let documentHeight = document.body.scrollHeight;
+
+  console.log('scrollHeight: ' + scrollHeight);
+  console.log('documentHeight: ' + documentHeight);
+
   //console.log(pageYOffset);
   if (pageYOffset >= 10) {
     header.classList.add('on');
@@ -72,6 +181,40 @@ function scrollFunc() {
       sidebox.removeAttribute('style');
     }
   }
+
+  if (scrollHeight >= documentHeight) {
+    let page = document.querySelector('#page').value;
+    document.querySelector('#page').value = parseInt(page) + 1;
+
+    callMorePostAjax(page);
+    if (page > 5) {
+      return;
+    }
+  }
+}
+
+function callMorePostAjax(page) {
+  if (page > 5) {
+    return;
+  }
+
+  $.ajax({
+    Method: 'POST',
+    url: './post.html',
+    data: {
+      'page': page,
+    },
+    dataType: 'html',
+    success: addMorePostAjax,
+    error: function (request, status, error) {
+      alert('문제가 발생했습니다.');
+      window.location.replace('https://www.naver.com');
+    }
+  });
+}
+
+function addMorePostAjax(data) {
+  deligation.insertAdjacentHTML('beforeend', data);
 }
 
 setTimeout(function () {
