c775de1755c63efe714399ab5eda727da2fc91ec
anifilm <anifilm02@gmail.com>
2021-12-25 11:45:09 +0900

    study django 2021-12-25

--- django/django-pinterest-inflearn/pinterest-clone/account/views.py
+++ django/django-pinterest-inflearn/pinterest-clone/account/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'base.html')
