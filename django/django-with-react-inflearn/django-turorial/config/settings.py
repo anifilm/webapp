4f4867e04e5543048ee4243b1952a6a4f9166108
anifilm <anifilm02@gmail.com>
2021-12-29 08:50:03 +0900

    Update settings.py

--- django/django-with-react-inflearn/django-turorial/config/settings.py
+++ django/django-with-react-inflearn/django-turorial/config/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICTILES_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / 'config' / 'static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
