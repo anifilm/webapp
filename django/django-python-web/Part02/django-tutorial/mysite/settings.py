e406ca6f5b22b6cba7f29c32757436d37d25bc0b
anifilm <anifilm02@gmail.com>
2021-12-29 08:50:01 +0900

    Update settings.py

--- django/django-python-web/Part02/django-tutorial/mysite/settings.py
+++ django/django-python-web/Part02/django-tutorial/mysite/settings.py
@@ -126,10 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 TAGGIT_CASE_INSENSITIVE = True
 TAGGIT_LIMIT = 50
