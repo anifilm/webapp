6866972f310eebfdc0d3c1ee1695ab364ecd4ae3
anifilm <anifilm02@gmail.com>
2021-12-31 11:28:24 +0900

    Update redis.js

--- node_js/codefirst_node_js/chap04/redis.js
+++ node_js/codefirst_node_js/chap04/redis.js
@@ -1,6 +1,7 @@
 const redis = require('redis');
-const client = redis.createClient(6379, '127.0.0.1');
+const client = redis.createClient(6379, 'localhost');
 
 client.get('myKey', (err, value) => {
   console.log(value);
+  client.quit();
 });
