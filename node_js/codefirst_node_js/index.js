ae8b6f0c877137c81b403af12ccae104a6e29d8c
anifilm <anifilm02@gmail.com>
2021-12-31 14:46:26 +0900

    Update index.js

--- node_js/codefirst_node_js/index.js
+++ node_js/codefirst_node_js/index.js
@@ -1,140 +1,31 @@
-const express = require('express');
-const morgan = require('morgan');
-const url = require('url');
-const uuidAPIkey = require('uuid-apikey');
-const cors = require('cors');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const dotenv = require('dotenv');
-dotenv.config();
-
-const app = express();
-
-// 포트 설정
-const port = process.env.PORT || 8000;
-app.set('port', port);
-
-// 공통 미들웨어
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors()); // 모든 라우터에 cors 적용
-
-// 테스트를 위한 API키
-const key = {
-  apiKey: process.env.apiKey,
-  uuid: process.env.uuid,
+const getHtml = async () => {
+  try {
+    return await axios.get('https://roadbook.co.kr/category/%EC%8B%A0%EA%B0%84%EC%86%8C%EA%B0%9C');
+  }
+  catch (err) {
+    console.error(err);
+  }
 };
 
-// 테스트를 위한 게시글 데이터
-let boardList = [
-  {
-    id: '1',
-    userId: '박민경',
-    title: '안녕하세요.',
-    content: '날씨가 많이 춥습니다. 건강하세요.',
-    date: '2021-12-31T03:12:24.479Z',
-  },
-  {
-    id: '2',
-    userId: '로드북',
-    title: '안녕하세요. 로드북 입니다.',
-    content: '백견불여일타 시리즈 많이 사랑해주세요.',
-    date: '2021-12-31T03:14:05.483Z',
-  },
-  {
-    id: '3',
-    userId: '김현정',
-    title: '벌써 연말이네요.',
-    content: '이번 년도는 특히 코로나 때문인지 시간이 순식간에 지나갔습니다. 앞으로...',
-  },
-];
-let numOfBoard = boardList.length;
+getHtml()
+  .then((html) => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $('div#searchList ol').children('li');
 
-// 라우팅 설정
-app.get('/', (req, res) => {
-  res.send('This is api.js');
-});
+    $bodyList.each(function (i, elem) {
+      ulList[i] = {
+        bookList: $(this).find('a').text(),
+        url: 'https://roadbook.co.kr' + $(this).find('a').attr('href'),
+      };
+    });
 
-app.get('/board', (req, res) => {
-  res.send(boardList);
-});
-
-app.post('/board', (req, res) => {
-  const board = {
-    id: ++numOfBoard + '', // 문자로 저장
-    userId: req.body.userId,
-    title: req.body.title,
-    content: req.body.content,
-    date: new Date(),
-  };
-  boardList.push(board);
-
-  res.redirect('/board');
-});
-
-app.put('/board/:id', (req, res) => {
-  // req.params.id 값 찾아 리스트에서 삭제
-  const findItem = boardList.find((item) => {
-    return item.id === req.params.id;
+    const data = ulList.filter((n) => n.bookList);
+    return data;
+  })
+  .then((res) => {
+    console.log(res);
   });
-
-  const idx = boardList.indexOf(findItem);
-  boardList.splice(idx, 1);
-
-  // 리스트에 새로운 요소 추가
-  const board = {
-    id: req.params.id,
-    userId: req.body.userId,
-    title: req.body.title,
-    content: req.body.content,
-    date: new Date(),
-  };
-  boardList.push(board);
-
-  res.redirect('/board');
-});
-
-app.delete('/board/:id', (req, res) => {
-  // req.params.id 값 찾아 리스트에서 삭제
-  const findItem = boardList.find((item) => {
-    return item.id === req.params.id;
-  });
-
-  const idx = boardList.indexOf(findItem);
-  boardList.splice(idx, 1);
-
-  res.redirect('/board');
-});
-
-// 게시글 검색 API using uuid-key
-app.get('/board/:apikey/:type', (req, res) => {
-  let { type, apikey } = req.params;
-  const queryData = url.parse(req.url, true).query;
-
-  if (uuidAPIkey.isAPIKey(apikey) && uuidAPIkey.check(apikey, key.uuid)) {
-    if (type === 'search') {
-      const keyword = queryData.keyword;
-      const result = boardList.filter((e) => {
-        return e.title.includes(keyword);
-      });
-      res.send(result);
-    }
-    else if (type === 'user') {
-      const userId = queryData.userId;
-      const result = boardList.filter((e) => {
-        return e.userId === userId;
-      });
-      res.send(result);
-    }
-    else {
-      res.send('wrong URL');
-    }
-  }
-  else {
-    res.send('wrong API Key');
-  }
-});
-
-app.listen(port, () => {
-  console.log(port + '포트에서 서버 실행중...');
-});
