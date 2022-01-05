2cc2d465e0d3c14d4ed2adb321ef0272503aae5c
anifilm <anifilm02@gmail.com>
2022-01-06 08:51:28 +0900

    Update app.js

--- node_js/Express/express-js-cli-app_nunjucks/app.js
+++ node_js/Express/express-js-cli-app_nunjucks/app.js
@@ -25,10 +25,10 @@ nunjucks.configure('views', {
 });
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
