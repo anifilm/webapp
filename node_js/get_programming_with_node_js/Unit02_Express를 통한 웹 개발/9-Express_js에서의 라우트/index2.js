10130fd7c3807f9beb3ea9e12de58a05a6e53458
anifilm <anifilm02@gmail.com>
2022-01-07 13:52:34 +0900

    study node.js 2022-01-07

--- node_js/get_programming_with_node_js/Unit02_Express를 통한 웹 개발/9-Express_js에서의 라우트/index2.js
+++ node_js/get_programming_with_node_js/Unit02_Express를 통한 웹 개발/9-Express_js에서의 라우트/index2.js
@@ -1,6 +1,9 @@
 const port = 3000,
-  express = require("express"),
+  express = require('express'),
   app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(`request made to: ${req.url}`);
@@ -11,11 +14,17 @@ app.get('/', (req, res) => {
   res.send('Hello, Universe!');
 });
 
+app.post('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.query);
+  res.send({ body: req.body, msg: 'POST Successful!' });
+});
+
 app.post('/contact', (req, res) => {
   res.send('Contact information submitted successfully.');
 });
 
-app.get("/items/:vegetable", (req, res) => {
+app.get('/items/:vegetable', (req, res) => {
   let veg = req.params.vegetable;
   res.send(`This is the page for ${veg}`);
 });
