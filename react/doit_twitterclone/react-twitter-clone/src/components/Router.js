f17818d7e8151c28698dd8fcbc54097c9292ce9e
anifilm <anifilm02@gmail.com>
2022-01-12 12:49:55 +0900

    study react 2022-01-12

---
+++ react/doit_twitterclone/react-twitter-clone/src/components/Router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
