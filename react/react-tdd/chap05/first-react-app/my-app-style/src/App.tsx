8793890bd941e0e922ba8b0f41e8e681f7be065a
anifilm <anifilm02@gmail.com>
2022-02-01 10:37:32 +0900

    study react 2022-02-01

--- react/react-tdd/chap05/first-react-app/my-app-style/src/App.tsx
+++ react/react-tdd/chap05/first-react-app/my-app-style/src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import Styled, { keyframes } from 'styled-components';
+
+const Container = Styled.div`
+  text-align: center;
+`;
+
+const Header = Styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = Styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${spin} infinite 20s linear;
+  }
+`;
+
+const AppLink = Styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Header>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </Header>
+    </Container>
   );
 }
 
