18c193876e3b436049aedb8f3f43520a082cbe67
anifilm <anifilm02@gmail.com>
2022-02-02 09:29:55 +0900

    Update index.js

--- node_js/js_everywhere/notedly/api/src/index.js
+++ node_js/js_everywhere/notedly/api/src/index.js
@@ -1,2 +1,7 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello, world!'));
+
+app.listen(4000, () => console.log('Listening on port 4000!'));
