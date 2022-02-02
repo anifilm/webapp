556abd8dce4d05a822c19f4d0d67f0d5be9d41b9
anifilm <anifilm02@gmail.com>
2022-02-02 14:44:52 +0900

    Update index.js

--- node_js/js_everywhere/notedly/api/src/index.js
+++ node_js/js_everywhere/notedly/api/src/index.js
@@ -3,30 +3,12 @@ const { ApolloServer, gql } = require('apollo-server-express');
 require('dotenv').config();
 
 const db = require('./db');
-const { models } = require('mongoose');
+const models = require('./models');
 
 // .env 파일에 명시된 포트 또는 포트 4000번에서 서버를 실행
-const port = process.env.POST || '4000';
+const port = process.env.PORT || 4000;
 // DB_HOST 값을 변수로 저장
 const DB_HOST = process.env.DB_HOST;
-
-let notes = [
-  {
-    id: '1',
-    content: 'This is a note',
-    author: 'Adam Scott',
-  },
-  {
-    id: '2',
-    content: 'This is another note',
-    author: 'Harlow Everly',
-  },
-  {
-    id: '3',
-    content: 'Oh hey look, another note!',
-    author: 'Riley Harrison',
-  },
-];
 
 // 그래프QL 스키마 언어로 스키마를 구성
 const typeDefs = gql`
@@ -54,8 +36,8 @@ const resolvers = {
     notes: async () => {
       return await models.Note.find();
     },
-    note: (parent, args) => {
-      return notes.find((note) => note.id === args.id);
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     },
   },
   Mutation: {
@@ -76,7 +58,7 @@ db.connect(DB_HOST);
 // 아폴로 서버 설정
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// 아폴로 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
+// 아폴러 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen({ port }, () =>
@@ -84,9 +66,3 @@ app.listen({ port }, () =>
     `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`,
   ),
 );
-
-/*app.get('/', (req, res) => res.send('Hello, world!'));
-
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`),
-);*/
