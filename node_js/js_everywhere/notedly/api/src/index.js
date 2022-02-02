f1f6e9811094371beb6ea60a2dbfb8f9970339e5
anifilm <anifilm02@gmail.com>
2022-02-02 16:38:26 +0900

    study express 2022-02-02

--- node_js/js_everywhere/notedly/api/src/index.js
+++ node_js/js_everywhere/notedly/api/src/index.js
@@ -1,64 +1,33 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
 
+// 로컬 모듈 임포트
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 // .env 파일에 명시된 포트 또는 포트 4000번에서 서버를 실행
 const port = process.env.PORT || 4000;
 // DB_HOST 값을 변수로 저장
 const DB_HOST = process.env.DB_HOST;
 
-// 그래프QL 스키마 언어로 스키마를 구성
-const typeDefs = gql`
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-
-  type Query {
-    hello: String
-    notes: [Note!]!
-    note(id: ID!): Note!
-  }
-
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`;
-
-// 스키마 필드를 위한 리졸버 함수 제공
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!',
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    },
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Adam Scott',
-      });
-    },
-  },
-};
-
 const app = express();
-
 // DB에 연결
 db.connect(DB_HOST);
 
 // 아폴로 서버 설정
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  content: () => {
+    // content에 db models 추가
+    return { models };
+  },
+});
 
-// 아폴러 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
+// 아폴러 그래프QL 미들웨어를 적용하고 경로를 '/api'로 설정
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen({ port }, () =>
