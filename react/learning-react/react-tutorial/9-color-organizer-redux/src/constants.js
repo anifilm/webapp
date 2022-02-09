69951cc88c1f955139cc13f53d11035ebf35cdd2
anifilm <anifilm02@gmail.com>
2022-02-09 19:07:48 +0900

    study react-redux 2022-02-09

---
+++ react/learning-react/react-tutorial/9-color-organizer-redux/src/constants.js
@@ -0,0 +1,6 @@
+export const constants = {
+  ADD_COLOR: 'ADD_COLOR',
+  RATE_COLOR: 'RATE_COLOR',
+  REMOVE_COLOR: 'REMOVE_COLOR',
+  SORT_COLORS: 'SORT_COLORS',
+};
---
+++ react/learning-react/react-tutorial/9-color-organizer-redux/src/store/reducers.js
@@ -0,0 +1,48 @@
+import { ADD_COLOR, RATE_COLOR, REMOVE_COLOR, SORT_COLORS } from './constants';
+
+export const color = (state={}, action) => {
+  switch (action.type) {
+    case ADD_COLOR:
+      return {
+        id: action.id,
+        title: action.title,
+        color: action.color,
+        timestamp: action.timestamp,
+        rating: 0,
+      };
+    case RATE_COLOR:
+      return state.id !== action.id
+        ? state
+        : {
+            ...state,
+            rating: action.rating
+          };
+    default:
+      return state;
+  }
+};
+
+export const colors = (state=[], action) => {
+  switch (action.type) {
+    case ADD_COLOR:
+      return [
+        ...state,
+        color({}, action)
+      ];
+    case RATE_COLOR:
+      return state.map((c) => color(c, action));
+    case REMOVE_COLOR:
+      return state.filter((c) => c.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+export const sort = (state='SORTED_BY_DATE', action) => {
+  switch (action.type) {
+    case SORT_COLORS:
+      return action.sortBy;
+    default:
+      return state;
+  }
+};
