189409caffb1e5123dcb2501fd5ff0b2d894c796
anifilm <anifilm02@gmail.com>
2022-07-29 22:48:22 +0900

    study django 2022-07-29

---
+++ django/django-rest-framework/ch2_photo/photo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.photo_list, name='photo_list'),
+    path('photo/<int:pk>/', views.photo_detail, name='photo_detail'),
+]
