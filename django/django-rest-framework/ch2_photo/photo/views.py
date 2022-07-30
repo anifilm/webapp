d29b46bf64fff01aa6b0851672af7766cba7ed98
anifilm <anifilm02@gmail.com>
2022-07-30 10:30:58 +0900

    study django 2022-07-30

--- django/django-rest-framework/ch2_photo/photo/views.py
+++ django/django-rest-framework/ch2_photo/photo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Photo
+from .forms import PhotoForm
 
 
 def photo_list(request):
@@ -10,3 +11,30 @@ def photo_list(request):
 def photo_detail(request, pk):
     photo = get_object_or_404(Photo, pk=pk)
     return render(request, 'photo/photo_detail.html', {'photo': photo})
+
+
+def photo_post(request):
+    if request.method == 'POST':
+        form = PhotoForm(request.POST)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.save()
+            return redirect('photo_detail', pk=photo.pk)
+    else:
+        form = PhotoForm()
+
+    return render(request, 'photo/photo_post.html', {'form': form})
+
+
+def photo_edit(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, instance=photo)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.save()
+            return redirect('photo_detail', pk=photo.pk)
+    else:
+        form = PhotoForm(instance=photo)
+
+    return render(request, 'photo/photo_edit.html', {'form': form, 'pk': pk})
