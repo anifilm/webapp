9e952f604cc495fce2f7823e29eb358f407eae68
anifilm <anifilm02@gmail.com>
2022-07-31 12:44:16 +0900

    study django 2022-07-31

--- django/django-rest-framework/ch4_DRF_basic/example/views.py
+++ django/django-rest-framework/ch4_DRF_basic/example/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from .models import Book
+from .serializsers import BookSerializser
 
-# Create your views here.
+'''
+class HelloAPI(APIView):
+    def get(self, request):
+        return Response('Hello world!)
+'''
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response('Hello world!')
+
+@api_view(['GET', 'POST'])
+def booksAPI(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializser(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BookSerializser(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def bookAPI(request, bid):
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializser(book)
+    return Response(serializer.data, status=status.HTTP_200_OK)
