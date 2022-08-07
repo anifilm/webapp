d59b7bf1d0c58edddd619aa77425e44f9d6c57b1
anifilm <anifilm02@gmail.com>
2022-08-07 14:33:24 +0900

    study django 2022-08-07

--- django/django-rest-framework/ch6_board/users/serializers.py
+++ django/django-rest-framework/ch6_board/users/serializers.py
@@ -1,3 +1,5 @@
+from typing_extensions import Required
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.http import request
@@ -39,3 +41,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
         token = Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {'error': 'Unable to log in with provided credentials.'}
+        )
