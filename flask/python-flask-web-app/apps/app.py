5de443c5a33f1486919b5cc53c9d7c1dbec798fb
anifilm <anifilm02@gmail.com>
2023-03-25 19:03:32 +0900

    study flask 2023-03-25

--- flask/python-flask-web-app/apps/app.py
+++ flask/python-flask-web-app/apps/app.py
@@ -5,23 +5,27 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
+from apps.config import config
+
 db = SQLAlchemy()  # SQLAlchemy를 인스턴스화한다.
 csrf = CSRFProtect()
 
 
 # create_app 함수를 작성한다.
-def create_app():
+def create_app(config_key):  # config의 키를 전달한다.
     app = Flask(__name__)  # 플라스크 인스턴스 생성
 
-    # 앱의 config 설정을 한다.
-    app.config.from_mapping(
-        SECRET_KEY="3RZSMss3p5QPbcY2hBsL",
-        SQLALCHEMY_DATABASE_URI="sqlite:///" + str(Path(Path(__file__).parent.parent, "local.sqlite")),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        # SQL을 콘솔 로그에 출력하는 설정
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="FuzzyszU5sugKN7KZs8f",
-    )
+    # 앱의 config 설정을 한다. (config 구성 이전 설정)
+    #app.config.from_mapping(
+    #    SECRET_KEY="3RZSMss3p5QPbcY2hBsL",
+    #    SQLALCHEMY_DATABASE_URI="sqlite:///" + str(Path(Path(__file__).parent.parent, "local.sqlite")),
+    #    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    #    # SQL을 콘솔 로그에 출력하는 설정
+    #    SQLALCHEMY_ECHO=True,
+    #    WTF_CSRF_SECRET_KEY="FuzzyszU5sugKN7KZs8f",
+    #)
+    # config_key에 매치하는 환경의 config 클래스를 읽어 들인다.
+    app.config.from_object(config[config_key])
 
     db.init_app(app)  # SQLAlchemy와 앱을 연계한다.
     Migrate(app, db)  # Migrate와 앱을 연계한다.
---
+++ flask/python-flask-web-app/apps/config.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+basedir = Path(__file__).parent.parent
+
+
+# BaseConfig 클래스 작성한다.
+class BaseConfig:
+    SECRET_KEY = "3RZSMss3p5QPbcY2hBsL"
+    WTF_CSRF_SECRET_KEY = "FuzzyszU5sugKN7KZs8f"
+
+
+# BaseConfig 클래스를 상속하여 LocalConfig 클래스를 작성한다.
+class LocalConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'local.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQL을 콘솔 로그에 출력하는 설정
+    SQLALCHEMY_ECHO = True
+
+
+# BaseConfig 클래스를 상속하여 TestingConfig 클래스를 작성한다.
+class TestingConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'testing.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+
+
+# config 사전에 매핑한다.
+config = {
+    "testing": TestingConfig,
+    "local": LocalConfig,
+}
