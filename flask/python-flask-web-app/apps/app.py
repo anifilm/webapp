debab7cc1a9ae6fe1166709d13945d01752db985
anifilm <anifilm02@gmail.com>
2023-03-25 18:49:52 +0900

    study flask 2023-03-25

--- flask/python-flask-web-app/apps/app.py
+++ flask/python-flask-web-app/apps/app.py
@@ -3,27 +3,30 @@ from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
-# SQLAlchemy를 인스턴스화한다.
-db = SQLAlchemy()
+db = SQLAlchemy()  # SQLAlchemy를 인스턴스화한다.
+csrf = CSRFProtect()
+
 
 # create_app 함수를 작성한다.
 def create_app():
-    # 플라스크 인스턴스 생성
-    app = Flask(__name__)
+    app = Flask(__name__)  # 플라스크 인스턴스 생성
 
     # 앱의 config 설정을 한다.
     app.config.from_mapping(
         SECRET_KEY="3RZSMss3p5QPbcY2hBsL",
-        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_DATABASE_URI="sqlite:///" + str(Path(Path(__file__).parent.parent, "local.sqlite")),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        # SQL을 콘솔 로그에 출력하는 설정
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY="FuzzyszU5sugKN7KZs8f",
     )
 
-    # SQLAlchemy와 앱을 연계한다.
-    db.init_app(app)
+    db.init_app(app)  # SQLAlchemy와 앱을 연계한다.
+    Migrate(app, db)  # Migrate와 앱을 연계한다.
 
-    # Migrate와 앱을 연계한다.
-    Migrate(app, db)
+    csrf.init_app(app)
 
     # crud 패키지로부터 views를 import한다.
     from apps.crud import views as crud_views
--- flask/python-flask-web-app/apps/crud/views.py
+++ flask/python-flask-web-app/apps/crud/views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
+
+from apps.app import db  # db를 import한다.
+from apps.crud.forms import UserForm
+from apps.crud.models import User  # User 클래스를 import한다.
 
 # Blueprint로 crud 앱을 생성한다.
 crud = Blueprint(
@@ -12,3 +16,62 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "콘솔 로그를 확인해 주세요."
+
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    # UserForm을 인스턴스화한다.
+    form = UserForm()
+    # 폼의 값을 검증한다.
+    if form.validate_on_submit():
+        # 사용자를 작성한다.
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        # 사용자를 추가하고 커밋한다.
+        db.session.add(user)
+        db.session.commit()
+        # 사용자의 일람 화면으로 리다이렉트한다.
+        return redirect(url_for("crud.users"))
+
+    return render_template("crud/create.html", form=form)
+
+
+@crud.route("/users")
+def users():
+    """사용자의 일람을 취득한다."""
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
+
+
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+    # User 모델을 이용하여 사용자를 취득한다.
+    user = User.query.filter_by(id=user_id).first()
+    # form으로부터 제출된 경우는 사용자를 갱신하여 사용자의 일람 화면으로 리다이렉트한다.
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+
+    # GET의 경우는 HTML을 반환한다.
+    return render_template("crud/edit.html", user=user, form=form)
+
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
