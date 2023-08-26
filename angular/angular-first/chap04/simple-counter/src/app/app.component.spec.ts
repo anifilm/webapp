84c8acf79b0ff3c776dfd350a6a4ef2a95e30c0d
anifilm <anifilm02@gmail.com>
2023-08-26 16:28:14 +0900

    study angular 2023-08-26

--- angular/angular-first/chap04/simple-counter/src/app/app.component.spec.ts
+++ angular/angular-first/chap04/simple-counter/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-angular'`, () => {
+  it(`should have as title 'simple-counter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-angular');
+    expect(app.title).toEqual('simple-counter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-counter app is running!');
   });
 });
